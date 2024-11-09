3268ffe9fb989b8ad3c0fd60ddbd0e12a9237b9f
Cmrandall86 <cmrandall86@gmail.com>
2024-11-08 22:46:49 -0700

    standalone?

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'standalone', // Use standalone for deployment flexibility if using dynamic routes
 };
 
 module.exports = nextConfig;
