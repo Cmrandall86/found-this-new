91c32df9e0d1157c85727e2c5b550bba68355f6f
Cmrandall86 <cmrandall86@gmail.com>
2024-11-08 22:08:04 -0700

    another try

--- next.config.js
+++ next.config.js
@@ -1,26 +1,10 @@
 /** @type {import('next').NextConfig} */
-const sanityClient = require('@sanity/client');
-
-// Create the Sanity client for fetching data
-const client = sanityClient({
-  projectId: process.env.SANITY_PROJECT_ID,
-  dataset: process.env.SANITY_DATASET,
-  useCdn: true,
-  token: process.env.SANITY_API_TOKEN, // Add token for authenticated requests if needed
-});
-
 const nextConfig = {
   reactStrictMode: true,
-  async exportPathMap(defaultPathMap) {
-    const paths = await client
-      .fetch('*[_type == "post"].slug.current')
-      .then(slugs =>
-        slugs.reduce((acc, slug) => {
-          acc[`/blog/${slug}`] = { page: '/blog', query: { slug } };
-          return acc;
-        }, { '/': { page: '/' } })
-      );
-    return paths;
+  env: {
+    SANITY_PROJECT_ID: process.env.SANITY_PROJECT_ID,
+    SANITY_DATASET: process.env.SANITY_DATASET,
+    SANITY_API_TOKEN: process.env.SANITY_API_TOKEN,
   },
 };
 
