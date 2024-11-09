fcf6796af16d7f01f7b253f8a311e8c361f02902
Cmrandall86 <cmrandall86@gmail.com>
2024-11-08 22:44:14 -0700

    try 200

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,17 +2,23 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import pluginReact from "eslint-plugin-react";
 
-/** @type {import('eslint').Linter.Config[]} */
+/** @type {import('eslint').Linter.Config} */
 export default [
-  { files: ["**/*.{js,mjs,cjs,jsx}"] },
-  {languageOptions: { globals: { ...globals.browser, process: "readonly" }}}, // Add this line
+  { 
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    languageOptions: {
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module", // Allows the use of import/export statements
+      },
+      globals: { ...globals.browser, process: "readonly" }
+    }
+  },
   pluginJs.configs.recommended,
-  pluginReact.configs.flat.recommended,
+  pluginReact.configs.recommended,
   {
     rules: {
       "react/prop-types": "off" // Turn off prop-types check
     }
   }
 ];
-
-
