c4a144df9ae9c4a50b79ba4a0eb535ca72538442
Cmrandall86 <cmrandall86@gmail.com>
2024-11-08 22:34:39 -0700

    another

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env: {
-    SANITY_PROJECT_ID: process.env.SANITY_PROJECT_ID,
-    SANITY_DATASET: process.env.SANITY_DATASET,
-    SANITY_API_TOKEN: process.env.SANITY_API_TOKEN,
-  },
 };
 
 module.exports = nextConfig;
