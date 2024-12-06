507eb87e90054d6e330c677bdc985a2f76b53079
Cmrandall86 <cmrandall86@gmail.com>
2024-12-05 18:27:02 -0700

    deploy environment variable try

--- src/components/ListOfFoundThings.js
+++ src/components/ListOfFoundThings.js
@@ -65,7 +65,7 @@ export default function ListOfFoundThings({ items, onDelete, onEdit }) {
           className="filter-input"
         />
         <select onChange={handleSortChange} className="sort-select" defaultValue="dateNewest">
-          <option value="title">Sort (A-Z)</option>
+          <option value="title">Sort (A to Z)</option>
           <option value="priceLow">Price Low to High</option>
           <option value="priceHigh">Price High to Low</option>
           <option value="dateNewest">Newest First</option>
