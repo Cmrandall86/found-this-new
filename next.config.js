573ed78ab114720b15df43fcbe4595f4191172ad
Cmrandall86 <cmrandall86@gmail.com>
2025-02-02 14:12:43 -0700

    4

--- next.config.js
+++ next.config.js
@@ -2,12 +2,35 @@
 const nextConfig = {
   reactStrictMode: true,
   output: 'standalone', // Use standalone for deployment flexibility if using dynamic routes
-  async headers() {
+  env: {
+    NEXT_PUBLIC_SANITY_PROJECT_ID: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+    NEXT_PUBLIC_SANITY_DATASET: process.env.NEXT_PUBLIC_SANITY_DATASET,
+    SANITY_API_TOKEN: process.env.SANITY_API_TOKEN,
+  },
+  // Disable all caching
+  generateEtags: false,
+  headers: async () => {
     return [
       {
         // Apply headers to all routes
         source: '/:path*',
         headers: [
+          {
+            key: 'Cache-Control',
+            value: 'no-store, no-cache, must-revalidate, proxy-revalidate',
+          },
+          {
+            key: 'Pragma',
+            value: 'no-cache',
+          },
+          {
+            key: 'Expires',
+            value: '0',
+          },
+          {
+            key: 'Surrogate-Control',
+            value: 'no-store',
+          },
           {
             key: 'Permissions-Policy',
             value: 'accelerometer=(), geolocation=(), microphone=(), camera=()', // Add valid policies
@@ -15,11 +38,6 @@ const nextConfig = {
         ],
       },
     ];
-  },
-  env: {
-    NEXT_PUBLIC_SANITY_PROJECT_ID: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-    NEXT_PUBLIC_SANITY_DATASET: process.env.NEXT_PUBLIC_SANITY_DATASET,
-    SANITY_API_TOKEN: process.env.SANITY_API_TOKEN,
   },
 };
 
